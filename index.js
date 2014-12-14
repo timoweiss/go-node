db5756b1155a9e3150902f726ed69cb9b9d8f5f5
Timo Weiß <info@timo-weiss.com>
2014-12-14 20:22:52 +0100

    add invokations

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ myGameField.setStone(5, 2);
 myGameField.setStone(2, 5);
 myGameField.setStone(1, 1);
 console.log('after:', myGameField.getCellStatus(1, 2));
-console.log(myGameField);
+console.log('after:', myGameField.gameField[1][1].setChecked());
+console.log(myGameField.gameField);
+console.log('after:', myGameField.resetAllChecked());
+console.log(myGameField.gameField);
