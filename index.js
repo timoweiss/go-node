e45438023a39f9338e37a36adeb2922b6d844733
Timo Weiß <info@timo-weiss.com>
2014-12-14 22:34:13 +0100

    naming

--- index.js
+++ index.js
@@ -3,22 +3,20 @@
 var Controller = require('./src/controller/controller');
 
 
-var myGameField = new Controller();
+var ctrl = new Controller();
 
 
-myGameField.createField(9);
+ctrl.createField(9);
+
+ctrl.setListeners();
+
+console.log('before:', ctrl.getCellStatus(1, 2));
 
 
-console.log('before:', myGameField.getCellStatus(1, 2));
+ctrl.setStone(1, 2);
+ctrl.setStone(3, 2);
+ctrl.setStone(5, 2);
+ctrl.setStone(2, 5);
+ctrl.setStone(1, 1);
 
-return;
-myGameField.setStone(1, 2);
-myGameField.setStone(3, 2);
-myGameField.setStone(5, 2);
-myGameField.setStone(2, 5);
-myGameField.setStone(1, 1);
-console.log('after:', myGameField.getCellStatus(1, 2));
-console.log('after:', myGameField.gameField[1][1].setChecked());
-console.log(myGameField.gameField);
-console.log('after:', myGameField.resetAllChecked());
-console.log(myGameField.gameField);
+console.log('after:', ctrl.getCellStatus(1, 2));
