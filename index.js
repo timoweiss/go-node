f1e7ffa1f7dccd7d3691868f51d621218a3e8160
Timo Weiß <info@timo-weiss.com>
2014-12-14 19:41:54 +0100

    testing

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ var GameField = require('./src/model/GameField');
 
 var myGameField = new GameField();
 myGameField.createGameField(9);
+console.log('before:', myGameField.getCellStatus(1, 2));
+myGameField.setStone(1, 2);
+myGameField.setStone(3, 2);
+myGameField.setStone(5, 2);
+myGameField.setStone(2, 5);
+myGameField.setStone(1, 1);
+console.log('after:', myGameField.getCellStatus(1, 2));
 console.log(myGameField);
