9a9e4d50e31ec839f818a9522126cf2a87427600
Timo Weiß <info@timo-weiss.com>
2014-12-14 22:37:23 +0100

    serve controller

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 
 var Controller = require('./src/controller/controller');
 
-
-var ctrl = new Controller();
-
-
-ctrl.createField(9);
-
-ctrl.setListeners();
-
-console.log('before:', ctrl.getCellStatus(1, 2));
+// var ctrl = new Controller();
 
 
-ctrl.setStone(1, 2);
-ctrl.setStone(3, 2);
-ctrl.setStone(5, 2);
-ctrl.setStone(2, 5);
-ctrl.setStone(1, 1);
+// ctrl.createField(9);
 
-console.log('after:', ctrl.getCellStatus(1, 2));
+// ctrl.setListeners();
+
+// console.log('before:', ctrl.getCellStatus(1, 2));
+
+
+// ctrl.setStone(1, 2);
+// ctrl.setStone(3, 2);
+// ctrl.setStone(5, 2);
+// ctrl.setStone(2, 5);
+// ctrl.setStone(1, 1);
+
+// console.log('after:', ctrl.getCellStatus(1, 2));
+module.exports = new Controller();
