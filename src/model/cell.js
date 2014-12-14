90a4fb2ea8950336eb8fdb8caa88f243ea764ca1
Timo Weiß <info@timo-weiss.com>
2014-12-14 19:46:00 +0100

    add more cell functionality

--- src/model/cell.js
+++ src/model/cell.js
@@ -17,4 +17,24 @@ Cell.prototype.setStatus = function(status) {
     this.status = status;
 };
 
+Cell.prototype.getChoords = function() {
+    return this.choords;
+};
+
+Cell.prototype.isChecked = function() {
+    return this.checked;
+};
+
+Cell.prototype.setChecked = function() {
+    this.checked = true;
+};
+
+Cell.prototype.resetChecked = function() {
+    this.checked = false;
+};
+
+Cell.prototype.toString = function() {
+    return this.choords.x + '|' + this.choords.y;
+};
+
 module.exports = Cell;
